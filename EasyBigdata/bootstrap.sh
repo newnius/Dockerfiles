630280e880d51146d6af43324562df85591f97a2
Newnius <newnius.cn@gmail.com>
2018-01-03 13:24:30 +0800

    update EasyBigdata since python3.5 has a bug in centos7

--- EasyBigdata/bootstrap.sh
+++ EasyBigdata/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-make apps
+: ${BOOTSTRAP_DIR:=/bootstrap}
+
+if [ -d "$BOOTSTRAP_DIR" ]; then
+	run-parts $BOOTSTRAP_DIR
+fi
 
 /var/www/html/build/env/bin/desktop runserver 0.0.0.0:8888
