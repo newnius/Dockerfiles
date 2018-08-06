e356a4af6c03ca78b8f50c0f08b4679382d508d0
Newnius <newnius.cn@gmail.com>
2018-08-06 19:07:59 +0800

    update hive

--- hive/2.1.1/bootstrap.sh
+++ hive/2.1.1/bootstrap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+## replace config
+: ${EXTRA_CONF_DIR:=/config/hive}
+
+if [ -d "$EXTRA_CONF_DIR" ]; then
+	cp $EXTRA_CONF_DIR/* /usr/local/hive/conf
+fi
+
 hive --service metastore
 
 if [[ $1 == "-d" ]]; then
