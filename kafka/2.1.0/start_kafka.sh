935402c7b77e5ea9047652ecba5ca3e1d92c1592
Newnius <newnius.cn@gmail.com>
2019-04-15 19:41:58 +0800

    update kafka to support configuration by environment variables

--- kafka/2.1.0/start_kafka.sh
+++ kafka/2.1.0/start_kafka.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-## Overide default configuration files
 : ${EXTRA_CONF_DIR:=/config/kafka}
 
+## Overide default configuration files
 if [ -d "$EXTRA_CONF_DIR" ]; then
 	cp $EXTRA_CONF_DIR/* /usr/local/kafka/config
+fi
+
+
+if ! [[ -z "${BROKER_ID}" ]]; then
+	sed -i '/broker.id=/d' /usr/local/kafka/config/server.properties
+	echo "broker.id=$BROKER_ID" >> /usr/local/kafka/config/server.properties
+fi
+
+if ! [[ -z "${ZOOKEEPER_CONNECT}" ]]; then
+	sed -i '/zookeeper.connect=/d' /usr/local/kafka/config/server.properties
+	echo "zookeeper.connect=$ZOOKEEPER_CONNECT" >> /usr/local/kafka/config/server.properties
 fi
 
 bash /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties
