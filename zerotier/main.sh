8658763a4c1da3c7458a70e3d130f48dca0e8c0a
Newnius <newnius.cn@gmail.com>
2019-11-06 15:38:29 +0800

    add zerotier

---
+++ zerotier/main.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
+
+if [ ! -e /dev/net/tun ]; then
+	echo 'FATAL: cannot start ZeroTier One in container: /dev/net/tun not present.'
+	exit 1
+fi
+
+exec "$@"
+
+
+if ! [[ -z "${NETWORK_ID}" ]]; then
+	zerotier-cli join ${NETWORK_ID}
+fi
